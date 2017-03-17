81deebbc96ef63f4df40c1d0121b78fd75b6a6c5
yozo06 <yogeshzope39@gmail.com>
2017-03-17 12:25:42 +0530

    Update download.js

--- download.js
+++ download.js
@@ -44,3 +44,68 @@ for(i=StarEpisode -1; i<=endEpisode - 1; i++)
 	};
 	episodeLinks.push('</table>')	
 }
+
+<script type='text/javascript' src='https://code.jquery.com/jquery-1.11.0.min.js'></script>
+//<!-- If you want to use jquery 2+: https://code.jquery.com/jquery-2.1.0.min.js -->
+<script type='text/javascript'>
+$(document).ready(function () {
+console.log("HELLO")
+function exportTableToCSV($table, filename) {
+var $headers = $table.find('tr:has(td)')
+
+// Temporary delimiter characters unlikely to be typed by keyboard
+// This is to avoid accidentally splitting the actual contents
+,tmpColDelim = String.fromCharCode(11) // vertical tab character
+,tmpRowDelim = String.fromCharCode(0) // null character
+// actual delimiter characters for CSV format
+,colDelim = '","'
+,rowDelim = '"\r\n"';
+// Grab text from table into CSV formatted string
+var csv = '"';
+csv += formatRows($headers.map(grabRow));
+csv += rowDelim;
+//csv += formatRows($rows.map(grabRow)) + '"';
+// Data URI
+var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+$(this)
+.attr({
+'download': filename
+,'href': csvData
+//,'target' : '_blank' //if you want it to open in a new window
+});
+//------------------------------------------------------------
+// Helper Functions
+//------------------------------------------------------------
+// Format the output so it has the appropriate delimiters
+function formatRows(rows){
+return rows.get().join(tmpRowDelim)
+.split(tmpRowDelim).join(rowDelim)
+.split(tmpColDelim).join(colDelim);
+}
+// Grab and format a row from the table
+function grabRow(i,row){
+var $row = $(row);
+//for some reason $cols = $row.find('td') || $row.find('th') won't work...
+var $cols = $row.find('td');
+if(!$cols.length) $cols = $row.find('th');
+return $cols.map(grabCol)
+.get().join(tmpColDelim);
+}
+// Grab and format a column from the table
+function grabCol(j,col){
+var $col = $(col),
+$text = $col.text();
+return $text.replace('"', '""'); // escape double quotes
+}
+}
+// This must be a hyperlink
+$("#export").click(function (event) {
+// var outputFile = 'export'
+var outputFile = window.prompt("What do you want to name your output file (Note: This won't have any effect on Safari)") || 'export';
+outputFile = outputFile.replace('.csv','') + '.csv'
+// CSV
+exportTableToCSV.apply(this, [$('#dvData>table'), outputFile]);
+// IF CSV, don't do event.preventDefault() or return false
+// We actually need this to be a typical hyperlink
+});
+});
