660b5a51a623ded9ab85ab9bdb23dbdf7ffa58e1
yozo06 <yogeshzope39@gmail.com>
2017-04-09 17:47:04 +0530

    Update download.js

--- download.js
+++ download.js
@@ -1,17 +1,29 @@
-var URL = window.location.origin
+function Fetch(){
+	var URL = window.location.origin
 
-var opportunity = $('div._3p9kRQ59EPzoyXOgTz7NYA a').map(function(i,el) { return $(el).attr('href'); });
-var op_url= URL+opportunity[0];
-window.open(op_url);
+	var opportunity = $('div._3p9kRQ59EPzoyXOgTz7NYA a').map(function(i,el) { return $(el).attr('href'); });
+	if(opportunity = 'undefined'){
+		console.log("currently not opportunity, but we will keep you updated....")
+	}
+	else{
+		console
+		var op_url= URL+opportunity[0];
+		window.open(op_url);
+		window.onload = function() {
+	  		var start_chat = document.getElementsByClassName("_2G_E8q0NNNYJBYrFTijbo6");
+			var Rate = start_chat[0];
+			Rate.click();
 
-var start_chat = document.getElementsByClassName("_2G_E8q0NNNYJBYrFTijbo6");
-var Rate = start_chat[0];
-Rate.click();
+			document.getElementById("offerMessageArea").value= "Hey I am Sanjali from India. I think I can help you. Please feel free to drop me a message. Thank you :)";
 
-document.getElementById("offerMessageArea").value= "Hey I am Sanjali from India. I think I can help you. Please feel free to drop me a message. Thank you :)";
+			var submit = document.getElementsByClassName("_2VhLTLyqvnfoTfiXiNyoFF");
+			var Send = start_chat[0];
+			Send.click();
 
-var submit = document.getElementsByClassName("_2VhLTLyqvnfoTfiXiNyoFF");
-var Send = start_chat[0];
-Send.click();
-
-window.close();
+			window.close();
+			};
+	}
+	
+var myVar = setInterval(function(){ Fetch() }, 1000);
+	
+}
